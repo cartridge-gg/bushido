84a268b5da37b6ae383efe56d3fc90f33bb2791e
bal7hazar <bal7hazar@proton.me>
2024-11-29 16:58:56 +0100

    🔥 Remove unused stuff

--- clients/arcade/src/main.tsx
+++ clients/arcade/src/main.tsx
@@ -1,33 +1,33 @@
-import React, { useCallback, useMemo } from "react";
+import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
-import { StarknetConfig, jsonRpcProvider } from "@starknet-react/core";
-import { Chain, sepolia } from "@starknet-react/chains";
-import { controller } from "./connectors/controller";
+// import { StarknetConfig, jsonRpcProvider } from "@starknet-react/core";
+// import { Chain, sepolia } from "@starknet-react/chains";
+// import { controller } from "./connectors/controller";
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement,
 );
 
 function Main() {
-  const { connectors } = useMemo(() => controller(), []);
-  const rpc = useCallback((_chain: Chain) => {
-    return { nodeUrl: import.meta.env.VITE_PUBLIC_NODE_URL };
-  }, []);
+  // const { connectors } = useMemo(() => controller(), []);
+  // const rpc = useCallback((_chain: Chain) => {
+  //   return { nodeUrl: import.meta.env.VITE_PUBLIC_NODE_URL };
+  // }, []);
 
   return (
     <React.StrictMode>
-      <StarknetConfig
+      {/* <StarknetConfig
         chains={[sepolia]}
         provider={jsonRpcProvider({ rpc })}
         connectors={connectors}
         autoConnect
-      >
+      > */}
         {/* <DojoProvider value={setupResult}> */}
           <App />
         {/* </DojoProvider> */}
-      </StarknetConfig>
+      {/* </StarknetConfig> */}
     </React.StrictMode>
   );
 }
